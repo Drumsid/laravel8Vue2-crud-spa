382eafae31bc04407a4c4a3901359eb17a718646
Drumsid <den-sidnay@yandex.ru>
2021-11-19 17:17:56 +0300

    add post data from api

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -4,30 +4,20 @@
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <!-- <th scope="col">№</th> -->
+                        <th scope="col">Title</th>
+                        <th scope="col">Text</th>
+                        <th scope="col">Created</th>
+                        <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
+                    <tr v-for="post in posts" :key="post">
+                        <!-- <td>1</td> -->
+                        <td>{{ post.title }}</td>
+                        <td>{{ post.post_text.substring(0, 35)}}...</td>
+                        <td>{{ post.created_at }}</td>
+                        <td></td>
                     </tr>
                 </tbody>
             </table>
@@ -37,7 +27,18 @@
 </template>
 
 <script>
-
+    export default {
+        data() {
+            return {
+                posts: []
+            }
+        },
+        mounted() {
+            axios.get('/api/posts').then(request => {
+                this.posts = request.data.data;
+            });
+        }
+    }
 </script>
 
 <style>
